c80409ada57dfe444944af7fea3cba4d023e3dcd
Andrew Nickerson <andrew.nickerson@chimp.net>
2016-10-01 16:08:42 -0700

    setting up oauth route for installing the app

--- config/default.js
+++ config/default.js
@@ -3,4 +3,5 @@ module.exports = {
   PORT : 3000,
   SLACK_CLIENT_ID: "86372351042.86357809904",
   SLACK_CLIENT_SECRET: "",
+  HOST_URI: "localhost"
 }
--- index.js
+++ index.js
@@ -4,32 +4,39 @@ var os = require('os');
 
 
 
+
 const clientSecret = config.SLACK_CLIENT_SECRET;
 const clientId = config.SLACK_CLIENT_ID;
 const port = config.PORT;
+const host = config.HOST_URI;
 
 if (!clientSecret || !clientId || !port) {
   console.log('Error: Specify clientId clientSecret and port in environment');
   process.exit(1);
 }
-
 var controller = Botkit.slackbot({
+  debug: true,
   json_file_store: './db_slackbutton_slashcommand/',
 }).configureSlackApp({
     clientId: clientId,
     clientSecret: clientSecret,
-    scopes: ['commands'],
+    redirectUri: `https://${host}/oauth`,
+    scopes: ['commands']
 });
 
-
-var controller = Botkit.slackbot({
-    debug: true
-});
 // receive outgoing or slash commands
 // if you are already using Express, you can use your own server instance...
 // see "Use BotKit with an Express web server"
 controller.setupWebserver(port,function(err,webserver) {
   controller.createWebhookEndpoints(controller.webserver);
+  controller.createHomepageEndpoint(controller.webserver)
+  controller.createOauthEndpoints(controller.webserver,function(err,req,res) {
+    if (err) {
+      res.status(500).send('ERROR: ' + err);
+    } else {
+      res.send('Success!');
+    }
+  });
 });
 
 controller.on('slash_command',function(bot,message) {
