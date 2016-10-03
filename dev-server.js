0dc241abd2fa4f8cde64dbb3155a4e602aea2ebf
Andrew Nickerson <andrew.nickerson@chimp.net>
2016-10-03 00:23:35 -0700

    dev server now starts up at the HOST_URI env variable for smoothing over app installation when testing locally

--- dev-server.js
+++ dev-server.js
@@ -2,8 +2,10 @@ var localtunnel = require('localtunnel');
 var config      = require('./lib/config-load')();
 var open = require('open');
 
+const hostURI = config.HOST_URI;
+const domainsArr = hostURI.split('.')
 
-var tunnel = localtunnel(config.PORT, {subdomain: 'jarbot'}, function(err, tunnel) {
+var tunnel = localtunnel(config.PORT, {subdomain: domainsArr[0]}, function(err, tunnel) {
     if (err)
     	console.log(err);
 
